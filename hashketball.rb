6dee206660db3f0fa55ebdbe128e55f963cddae0
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-24 03:48:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,16 +7,17 @@ def game_hash
   :home{
     :team_name => 'Brooklyn Nets'
     :colors => 'Black', 'White'
-    :players => [{
-        number: 4,
+    :players => [
+      { number: 4,
         shoe: 18,
         points: 10,
         rebounds: 1,
         assists: 1,
         steals: 2,
         blocks: 7,
-        slam_dunks: 2
-      }]
+        slam_dunks: 2 },
+        
+      ]
   }
   :away{
     :team_name
